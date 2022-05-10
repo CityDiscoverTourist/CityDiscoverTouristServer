676487c420b2f8725d1e817e722cc3161d83fdd9
Le Quoc Dat <dathaha2000@gmail.com>
2022-05-10 20:31:04 +0700

    remove credential

--- CityDiscoverTourist.API/Program.cs
+++ CityDiscoverTourist.API/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 var env = builder.Environment.EnvironmentName;
 var appName = builder.Environment.ApplicationName;
 
-builder.Configuration.AddSecretsManager(credentials: new AnonymousAWSCredentials(), region: RegionEndpoint.APSoutheast1, configurator: options =>
+builder.Configuration.AddSecretsManager(region:RegionEndpoint.APSoutheast1, configurator: options =>
 {
     //arn:aws:secretsmanager:ap-southeast-1:958841795550:secret:Production_CityDiscoverTourist.API_ConnectionStrings__DefaultConnection-65jWxM
     options.SecretFilter = entry => entry.Name.StartsWith($"{env}_{appName}_");
