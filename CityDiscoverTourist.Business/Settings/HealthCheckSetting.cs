6a8f4638b643fd1f6493de2a0a17a66832d1fbdd
Le Quoc Dat <dathaha2000@gmail.com>
2022-05-11 21:56:09 +0700

    trigger deployment

--- CityDiscoverTourist.Business/Settings/HealthCheckSetting.cs
+++
@@ -1,8 +0,0 @@
-namespace CityDiscoverTourist.Business.HealthCheck;
-
-public class HealthCheckSetting
-{
-    public string? Status { get; set; }
-    public IEnumerable<HealthCheck> Checks { get; set; }
-    public TimeSpan Duration { get; set; }
-}
