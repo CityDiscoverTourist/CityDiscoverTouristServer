ec693a2013277e0f2b0479f07e16cbcb89c28156
Le Quoc Dat <dathaha2000@gmail.com>
2022-05-11 10:36:30 +0700

    trigger deployment

--- CityDiscoverTourist.API/Program.cs
+++ CityDiscoverTourist.API/Program.cs
@@ -10,13 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 var env = builder.Environment.EnvironmentName;
 var appName = builder.Environment.ApplicationName;
 
-builder.Configuration.AddSecretsManager(region: RegionEndpoint.APSoutheast1, configurator: options =>
+/*builder.Configuration.AddSecretsManager(region: RegionEndpoint.APSoutheast1, configurator: options =>
 {
     //arn:aws:secretsmanager:ap-southeast-1:958841795550:secret:Production_CityDiscoverTourist.API_ConnectionStrings__DefaultConnection-65jWxM
     options.SecretFilter = entry => entry.Name.StartsWith($"{env}_{appName}_");
     options.KeyGenerator = (_, s) => s.Replace($"{env}_{appName}_", string.Empty)
         .Replace("__", ":");
-});
+});*/
 
 const string managedNetworkingAppContextSwitch = "Switch.Microsoft.Data.SqlClient.UseManagedNetworkingOnWindows";
 AppContext.SetSwitch(managedNetworkingAppContextSwitch, true);
