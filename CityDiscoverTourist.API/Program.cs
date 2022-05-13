5a3c7502c50a93afd22ead1d64f9d05ce23ad28c
Le Quoc Dat <dathaha2000@gmail.com>
2022-05-13 10:18:42 +0700

    trigger deployment

--- CityDiscoverTourist.API/Program.cs
+++ CityDiscoverTourist.API/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var env = builder.Environment.EnvironmentName;
 var appName = builder.Environment.ApplicationName;
-builder.Configuration.AddSecretsManager(credentials: new BasicAWSCredentials("AKIA56P3MWPPHUECOGQA","MCfQLg7Y9fZhSSJam5lzykcG//BZoOzAYLz6MXCm"), region: RegionEndpoint.APSoutheast1, configurator: options =>
+builder.Configuration.AddSecretsManager(credentials: new BasicAWSCredentials("AKIA56P3MWPPNG2QASFK","RVQWOHq+aD8ayQT1kK4VCqchDC8VSbJujIG2Z4jq"), region: RegionEndpoint.APSoutheast1, configurator: options =>
 {
     //arn:aws:secretsmanager:ap-southeast-1:958841795550:secret:Production_CityDiscoverTourist.API_ConnectionStrings__DefaultConnection-65jWxM
     options.SecretFilter = entry => entry.Name.StartsWith($"{env}_{appName}_");
