7a1b4156035d8c00c65ea4ffd71a32bfa68a4eab
Le Quoc Dat <dathaha2000@gmail.com>
2022-06-09 11:49:41 +0700

    clean up

--- CityDiscoverTourist.Business/IServices/ICustomerQuestService.cs
+++ CityDiscoverTourist.Business/IServices/ICustomerQuestService.cs
@@ -1,9 +1,7 @@
 using CityDiscoverTourist.Business.Data.RequestModel;
 using CityDiscoverTourist.Business.Data.ResponseModel;
-using CityDiscoverTourist.Business.Enums;
 using CityDiscoverTourist.Business.Helper;
 using CityDiscoverTourist.Business.Helper.Params;
-using CityDiscoverTourist.Data.Models;
 
 namespace CityDiscoverTourist.Business.IServices;
 
@@ -12,7 +10,6 @@ public interface ICustomerQuestService
     public PageList<CustomerQuestResponseModel> GetAll(CustomerQuestParams @params);
     public Task<CustomerQuestResponseModel> Get(int id);
     public Task<CustomerQuestResponseModel> CreateAsync(CustomerQuestRequestModel request);
-    public Task<CustomerQuestResponseModel> UpdateAsync(CustomerQuestRequestModel request);
     public Task<CustomerQuestResponseModel> DeleteAsync(int id);
-    public Task<CustomerQuestResponseModel> UpdateEndPointAndStatusWhenFinishQuestAsync(int id);
+    public Task<CustomerQuestResponseModel> UpdateEndPointAndStatusWhenFinishQuestAsync(int customerQuestId);
 }
