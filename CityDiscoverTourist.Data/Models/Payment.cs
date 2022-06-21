e0a0cbed7976bc159f9b0c4ed86376183cde5685
Le Quoc Dat <dathaha2000@gmail.com>
2022-06-21 19:29:39 +0700

    entity_modify

--- CityDiscoverTourist.Data/Models/Payment.cs
+++ CityDiscoverTourist.Data/Models/Payment.cs
@@ -4,6 +4,8 @@ public class Payment: BaseEntity
 {
     public string? PaymentMethod { get; set; }
     public string? Status { get; set; }
+    public int Quantity { get; set; }
+    public float TotalAmount { get; set; }
 
     public CustomerQuest? CustomerQuest { get; set; }
     public int CustomerQuestId { get; set; }
