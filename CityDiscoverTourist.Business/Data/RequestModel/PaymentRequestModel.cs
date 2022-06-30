99df0aefaaa6f8d95ee2ec754429f0e2d0a8afac
Le Quoc Dat <dathaha2000@gmail.com>
2022-06-30 12:40:06 +0700

    add dto payment

--- CityDiscoverTourist.Business/Data/RequestModel/PaymentRequestModel.cs
+++ CityDiscoverTourist.Business/Data/RequestModel/PaymentRequestModel.cs
@@ -2,11 +2,12 @@ namespace CityDiscoverTourist.Business.Data.RequestModel;
 
 public class PaymentRequestModel
 {
+    public Guid Id { get; set; }
     public string? PaymentMethod { get; set; }
-    public string? Status { get; set; }
     public int Quantity { get; set; }
-    public float TotalAmount { get; set; }
-
+    public double AmountTotal { get; set; }
+    public string? Status { get; set; }
     public string? CustomerId { get; set; }
+
     public int QuestId { get; set; }
 }
