895be91dfccb010abf9784b0196579ac192b929a
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-07-01 19:36:02 +0700

    modify LocationTypeResponseModel

--- CityDiscoverTourist.Business/Data/ResponseModel/LocationTypeResponseModel.cs
+++ CityDiscoverTourist.Business/Data/ResponseModel/LocationTypeResponseModel.cs
@@ -5,4 +5,6 @@ public class LocationTypeResponseModel
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Status { get; set; }
+
+    public List<LocationResponseModel>? Locations { get; set; }
 }
