ce58decf6d0db0d4435afacf7fd97fd768bb4866
Le Quoc Dat <dathaha2000@gmail.com>
2022-07-29 16:16:42 +0700

    remove unused field

--- CityDiscoverTourist.Business/Data/RequestModel/CustomerQuestRequestModel.cs
+++ CityDiscoverTourist.Business/Data/RequestModel/CustomerQuestRequestModel.cs
@@ -2,10 +2,6 @@ namespace CityDiscoverTourist.Business.Data.RequestModel;
 
 public class CustomerQuestRequestModel
 {
-    public int Rating { get; set; }
-    public string? FeedBack { get; set; }
-
     public string? CustomerId { get; set; }
-    public int QuestId { get; set; }
     public Guid PaymentId { get; set; }
 }
